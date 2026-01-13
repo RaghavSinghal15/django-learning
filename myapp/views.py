e1768db58a663973be1878bcc5e4943aa6a780be
Raghav Singhal <raghav.singhal1511@gmail.com>
2026-01-14 03:47:10 +0530

    Complete word counter functionality

--- myapp/views.py
+++ myapp/views.py
@@ -3,10 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    context = {
-        'name': 'patrick',
-        'age' : 23,
-        'nationality':'british',
-    };
-    return render(request,"index.html",context)
+    return render(request,"index.html")
     
+def counter(request):
+    text = request.GET['text']
+    amount_of_wrds = len(text.split())
+    return render(request,'counter.html',{'amt':amount_of_wrds})
